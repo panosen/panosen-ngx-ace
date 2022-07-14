69cb03c7c9e061463251395eca53df90a500a003
hc_zhang <hc_zhang@trip.com>
2022-07-14 21:00:49 +0800

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxAceModule } from '../../projects/pano/ngx-ace/src/public-api';
+import { NgxAceModule } from '../../projects/panosen-ngx-ace/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
